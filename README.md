93c95d7b60f8ca3856ea23fb78cf026ed5478182
jvillmo <jvillmo@gmail.com>
2025-07-21 16:14:32 -0600

    Refine movie fetch example

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# hola-mundo-react
-Created with CodeSandbox
+# Hola Mundo React
+
+Este proyecto fue actualizado para usar **React 18** y las últimas
+dependencias de *react-router-dom*. Ahora la aplicación hace uso de
+componentes funcionales y *hooks* para manejar el estado y las
+peticiones a la API de películas. Redux ha sido eliminado ya que el
+ejemplo es pequeño y la gestión de estado se resuelve de forma local.
+
+Para la clave de la API se utiliza una variable de entorno llamada
+`REACT_APP_API_KEY`. Copia el archivo `.env.sample` a `.env` y asigna tu
+propia clave antes de iniciar la aplicación.
+
+## Scripts disponibles
+
+```bash
+npm install
+npm start
+npm test
+```
+
+`npm start` ejecuta el proyecto en modo desarrollo.
+`npm test` corre la suite de pruebas si está configurada.
--- src/components/movieList.js
+++ src/components/movieList.js
@@ -1,131 +1,84 @@
-import React from "react";
-//import PropTypes from "prop-types";
-import { connect } from "react-redux";
-
+import React, { useEffect, useState } from "react";
 import MovieItem from "./movieItem";
 
-class MoviesList extends React.Component {
-  state = {
-    query: ""
-  };
+const APIKEY = process.env.REACT_APP_API_KEY;
 
-  componentDidMount() {
-    console.log("componentDidMount");
-    if (!this.props.movies || this.props.movies.moviesResults.length == 0) {
-      this.props.loadMovies();
-    } else {
-      this.setState({
-        query: this.props.movies.query
-      });
+export default function MovieList() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [lastLoad, setLastLoad] = useState(null);
+
+  const fetchMovies = async (search) => {
+    setLoading(true);
+    setError(null);
+    let url = `https://api.themoviedb.org/3/${search ? "search/movie" : "movie/now_playing"}?api_key=${APIKEY}&language=es-MX`;
+    if (search) {
+      url += `&query=${encodeURIComponent(search)}`;
     }
-  }
-
-  componentDidUpdate(prevProps) {
-    console.log("componentDidUpdate");
-  }
-
-  LookupForMovies = () => {
-    let d = this.props.loadMovies(this.state.query);
-    console.log(d);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results || []);
+      setLastLoad(new Date().toLocaleString());
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  render() {
-    const { movies } = this.props;
+  useEffect(() => {
+    fetchMovies("");
+  }, []);
 
-    // console.log("render MoviesList", this.props);
-    return (
-      <>
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="basic-addon3">
-              Nombre
-            </span>
-            <input
-              className="form-control"
-              type="text"
-              value={this.state.query}
-              onKeyPress={(event) => {
-                if (event.charCode == 13) {
-                  this.LookupForMovies();
-                }
-              }}
-              onChange={(event) => {
-                this.setState({ query: event.target.value });
-              }}
-              placeholder="Ingrese aqui la película que desea buscar"
-            />
-          </div>
-          <button className="btn btn-primary" onClick={this.LookupForMovies}>
-            Load Movies
-          </button>
+  const handleSearch = () => {
+    fetchMovies(query);
+  };
+
+  return (
+    <>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon3">
+            Nombre
+          </span>
+          <input
+            className="form-control"
+            type="text"
+            value={query}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                handleSearch();
+              }
+            }}
+            onChange={(event) => setQuery(event.target.value)}
+            placeholder="Ingrese aqui la película que desea buscar"
+          />
         </div>
+        <button className="btn btn-primary" onClick={handleSearch}>
+          Load Movies
+        </button>
+      </div>
 
-        {movies && movies.moviesResults && movies.moviesResults.length > 0 ? (
-          <>
-            <>
-              {movies.query ? (
-                <h1>Resultados para la busqueda {movies.query}</h1>
-              ) : (
-                <h1>Hoy en Cartelera</h1>
-              )}
-            </>
+      {loading && <h1>Cargando...</h1>}
+      {!loading && movies.length > 0 ? (
+        <>
+          <h1>
+            {query ? `Resultados para la busqueda ${query}` : "Hoy en Cartelera"}
+          </h1>
 
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-              {movies.moviesResults.map((movie, i) => (
-                <MovieItem key={movie.id} movie={movie} />
-              ))}
-            </div>
+          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+            {movies.map((movie) => (
+              <MovieItem key={movie.id} movie={movie} />
+            ))}
+          </div>
 
-            {/*
-            <ul>
-              {movies.moviesResults.map((movie, i) => (
-                <li key={movie.id}>
-                  <img
-                    src={"https://image.tmdb.org/t/p/w154/" + movie.poster_path}
-                    alt={movie.title}
-                  />
-                  <a to={"/play/player/" + movie.id}>{movie.title}</a>
-                </li>
-              ))}
-            </ul>
-          */}
-            <span>Última actualizacion: {movies.lastLoad}</span>
-          </>
-        ) : (
-          <>
-            <h1>SIN RESULTADOS</h1>
-            {movies && movies.error ? (
-              <h1>{movies.errorMessage.message}</h1>
-            ) : null}
-          </>
-        )}
-      </>
-    );
-  }
+          <span>Última actualizacion: {lastLoad}</span>
+        </>
+      ) : null}
+      {error && <h1>{error.message}</h1>}
+    </>
+  );
 }
-/*
-MoviesList.propTypes = {
-  movies: PropTypes.object.isRequired,
-  loadMovies: PropTypes.func.isRequired
-};
-*/
-const mapStateToProps = ({ movies }) => {
-  return {
-    movies
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    loadMovies: (query) => {
-      dispatch({ type: `LOADMOVIES`, query });
-    }
-  };
-};
-
-const ConnectedMoviesList = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(MoviesList);
-
-export default ConnectedMoviesList;
